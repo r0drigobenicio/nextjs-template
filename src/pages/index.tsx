d1ee85bad09888ff1719b11f27c992944bdf9bf5
Rodrigo Benício <rodrigobeniciosilva@gmail.com>
2021-07-21 20:13:37 -0300

    add: typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início</title>
+      </Head>
+
+      <h1>Hello, world!</h1>
+    </>
+  )
+}
