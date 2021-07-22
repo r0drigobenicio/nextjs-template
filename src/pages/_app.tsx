4e97fefdba99574dac8c41a01dc72644bd514fc0
Rodrigo Benício <rodrigobeniciosilva@gmail.com>
2021-07-22 10:55:06 -0300

    add: styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 
+import GlobalStyles from '../styles/GlobalStyles';
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default MyApp;
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #__next {
+    max-width: 100%;
+    max-height: 100%;
+  }
+
+  *, button, input {
+    border: 0;
+    background: none;
+    outline-color: var(--black-color);
+  }
+
+  body {
+    color: var(--black-color);
+    font: 400 1.6rem Roboto, sans-serif;
+  }
+
+  html {
+    background: var(--background-color);
+  }
+
+  button {
+    user-select: none;
+  }
+
+  :root {
+    font-size: 60%;
+
+    --background-color: #f5f5f5;
+    --black-color: #000;
+  }
+
+  @media (min-width: 768px) {
+    :root {
+      font-size: 62.5%;
+    }
+  }
+`;
